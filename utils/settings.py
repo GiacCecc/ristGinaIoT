919bd30efa536909eb7771c746a865005f44585e
GiacCecc <79569028+GiacCecc@users.noreply.github.com>
2023-05-08 21:42:42 +0200

    Update settings.LOG_TIMEDELTA
    settings.LOG_TIMEDELTA changed --> first attempt for OTA

--- utils/settings.py
+++ utils/settings.py
@@ -6,5 +6,5 @@
 TEMP_MAX = 13         # Max cold room temperature
 MAX_NLOGS_NIR = 10    # Max number of logs not in range
 
-LOG_TIMEDELTA = 1*60    # Timedelta between logs in sec.
+LOG_TIMEDELTA = 2*60    # Timedelta between logs in sec.
 MSG_TIMEDELTA = 3600  # Max timedelta from last WhatsApp message sent in sec.
