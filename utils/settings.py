45262679889cdaf0c00328c693fce7f23127e1ac
GiacCecc <79569028+GiacCecc@users.noreply.github.com>
2023-05-08 20:42:00 +0200

    Add files via upload

---
+++ utils/settings.py
@@ -0,0 +1,10 @@
+
+# SETTINGS
+
+#                                 SYSTEM SETTINGS
+# -----------------------------------------------
+TEMP_MAX = 13         # Max cold room temperature
+MAX_NLOGS_NIR = 10    # Max number of logs not in range
+
+LOG_TIMEDELTA = 1*60    # Timedelta between logs in sec.
+MSG_TIMEDELTA = 3600  # Max timedelta from last WhatsApp message sent in sec.
