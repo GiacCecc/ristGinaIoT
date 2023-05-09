d1a988e482bddf83b7cc240458a084ff31d09660
GiacCecc <79569028+GiacCecc@users.noreply.github.com>
2023-05-09 22:38:23 +0200

    Update umain.py

--- umain.py
+++ umain.py
@@ -8,17 +8,28 @@
 from time import sleep_ms
 from machine import Pin
 
+def blinkk(led):
+    for i in range(3):
+        led.on()
+        sleep_ms(500)
+        led.off()
+        sleep_ms(500)
+        
 def body():
     
     ledb = Pin(4, Pin.OUT)
     ledg = Pin(19, Pin.OUT)
     
-    print('LEDs are on...')
     ledb.on()
     ledg.on()
     sleep_ms(3000)
-    
-    print('LEDs are off!\n')
     ledb.off()
     ledg.off()
-    sleep_ms(2500)
+    sleep_ms(3000)
+    
+    
+    print('BLUE is blinking...')
+    blinkk(ledb)
+    
+    print('GREEN is blinking...')
+    blinkk(ledg)
