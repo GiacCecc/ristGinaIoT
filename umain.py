5d655dd740eb77012feccc14c4eb27de3d929140
GiacCecc <79569028+GiacCecc@users.noreply.github.com>
2023-05-13 16:41:40 +0200

    Add files via upload

---
+++ umain.py
@@ -0,0 +1,77 @@
+#
+# Giacomo Ceccarelli
+# Berlin, 11.05.2023
+# v 0.2
+# ---------------------------------------------------------
+
+from time import ticks_ms
+start_time = ticks_ms()
+
+from machine import Pin
+from time import sleep_ms
+
+import onewire, ds18x20
+
+from utils.secrets import MONGO_SRC, WHAT_PHONE_NR, WHAT_API_KEY
+from utils.settings import TEMP_MAX, MAX_NLOGS_NIR, MSG_TIMEDELTA
+
+import libs.uutil as util
+import libs.uwhat as what
+import libs.umongo as mongo
+import libs.ugit as ugit
+
+
+def check_update(pprint=False):
+    OTA_TRIGGER = mongo.get_OTA_TRIGGER(pprint=pprint)
+    print('\n-------------------------------------------\n')
+    print(f'OTA_TRIGGER is set to {OTA_TRIGGER} ---+ ', end='')
+
+    if OTA_TRIGGER:
+        print('It´s time to UPDATE Over The Air (OTA)...\n')
+        sleep_ms(2000)
+        mongo.update_OTA_TRIGGER(value=False, pprint=pprint)
+        mongo.insert_update(pprint=pprint)
+        ugit.pull_all(isconnected=True)
+    else:
+        print('NO UPDATE NOW +-\n')
+        sleep_ms(1000)
+    
+def body(pprint=False):
+    #i = 1/0
+    
+    # Instatiate DS18B20
+    # -------------------------------------------------------------------
+    ds = ds18x20.DS18X20(onewire.OneWire(Pin(22)))
+    roms = ds.scan()
+    
+    ds.convert_temp()
+    sleep_ms(2000)
+    
+    temp = ds.read_temp(roms[0])
+    print('Temperature (DS18B20): {:.2f} °C'.format(temp))
+    mongo.insert_temp(temp, pprint=pprint)
+    print('-Temperature inserted into MongoDB')
+    
+    # --------------- CHECK LOGS AND SEND WHATMSG IF WHATMSG_CONDS
+    if temp >= TEMP_MAX:
+        print('--Temperature ≥ {TEMP_MAX}')
+        temps = mongo.last_nlogs(MONGO_SRC.Collections.logs,
+                                 MAX_NLOGS_NIR,
+                                 'temp',
+                                 pprint=pprint)
+        
+        if all([i >= TEMP_MAX for i in temps]):
+            print('---All last {MAX_NLOGS_NIR} temperatures ≥ {TEMP_MAX}')
+            timedelta = what.timedelta_last()
+            
+            if timedelta >= MSG_TIMEDELTA:
+                print('---Timedelta from last whatmsg: {timedelta} ≥ {MSG_TIMEDELTA}')
+                msg = 'Temperature: {:.2f}, last WhatsApp sent {} sec. ago.'.format(temp, timedelta)
+                what.sendmsg(WHAT_PHONE_NR,
+                             WHAT_API_KEY,
+                             util.url_encode(msg),
+                             pprint=pprint)
+                print('-- whatmsg sent...')
+                mongo.insert_msg(msg, pprint=pprint)
+                print('- whatmsg inserted into MongoDB\n')
+                
