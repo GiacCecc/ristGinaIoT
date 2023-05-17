03c572fa0f081360dc45b65db09f2601ad8d2339
GiacCecc <79569028+GiacCecc@users.noreply.github.com>
2023-05-17 20:16:51 +0200

    Update settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -4,7 +4,7 @@
 #                                 SYSTEM SETTINGS
 # -----------------------------------------------
 TEMP_MAX = 6          # Max cold room temperature
-MAX_NLOGS_NIR = 10    # Max number of logs not in range
+MAX_NLOGS_NIR = 6    # Max number of logs not in range
 
 LOG_TIMEDELTA = 5*60  # Timedelta between logs in sec.
 MSG_TIMEDELTA = 3600  # Max timedelta from last WhatsApp message sent in sec.
